9a5d0734bf02285f49009a2b13953f4087d1cfcd
tharunprasanth <vivekanandan@ku.edu>
2017-09-17 20:09:45 -0700

    indendation correction changes include 1)closing the file after writing 2)changing the log file format 3)check for file availability in else that includes writing to the file

--- udp-recv.c
+++ udp-recv.c
@@ -24,6 +24,7 @@
  **************************************************************************/
 #define PORT 8
 #define BUFSIZE 100*1024*4
+#define SD_CARD_LOCATION "/media/ubuntu/disk/stack_radar"
 
 
 /**************************************************************************
@@ -50,6 +51,7 @@ int main(int argc, char **argv)
 	int port = PORT;
 	int packets = 0;                /* packet id */
 	FILE *fp;                       /* file name */
+	struct timeval tv;
 
 	if (argc == 2) {
 		port = atoi(argv[1]);
@@ -63,12 +65,10 @@ int main(int argc, char **argv)
 	}
 
 	/* bind the socket to any valid IP address and a specific port */
-
 	memset((char *)&myaddr, 0, sizeof(myaddr));
 	myaddr.sin_family = AF_INET;
 	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	myaddr.sin_port = htons(PORT);
-
 	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
 		perror("bind failed");
 		return 0;
@@ -77,25 +77,37 @@ int main(int argc, char **argv)
 	/* now loop, receiving data and printing what we received */
 	fprintf(stderr, "start main loop: waiting on port %d\n", PORT);  
 	for (;;) {
+		
 		recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
-		// fprintf(stderr, "received %d bytes\n", recvlen);
+		printf( "received %d bytes\n", recvlen);
+
 		if (recvlen > 0) {
 			buf[recvlen] = 0;
 
 			if (!strncmp(buf, "Ping", 4) || !strncmp(buf, "Pong", 4) ) {
 				time_t rawtime;
 				struct tm * timeinfo;
-				
 				/* session init */
 				packets = 0;
 
 				time (&rawtime);
 				timeinfo = localtime (&rawtime);
-				
-				strftime(fname, 80, "%Y-%m-%d_%H-%M-%S", timeinfo);
-		  
+
+				char end_text[80];
+				strftime(end_text, 80, "%Y-%m-%d_%H-%M-%S", timeinfo);
+
+				gettimeofday(&tv,NULL);
+				int milli_sec = tv.tv_usec/1000;
+
+				/*  SD card recording   */
+				if(fname[0]!='\0')
+					fclose(fp);
+				sprintf(fname,"%s/%s_%03d.bin",SD_CARD_LOCATION,end_text,milli_sec);
+
+				/* Tegra flash memory recording   */
+				printf("\n the file name is %s",fname);
 				fprintf(stderr, "received ping/pong. open %s\n", fname);
-		  
+		  		
 				fp = fopen(fname, "w+");
 				if (!fp) {
 					perror("open failed");
@@ -104,9 +116,10 @@ int main(int argc, char **argv)
 			} else {
 				/* data packet */
 				packets++;
-		  
-				int ret = fwrite(buf, 1, recvlen, fp);
+				if(fp){
+					int ret = fwrite(buf, 1, recvlen, fp);}
 			}
+				
 		}
 	}
 	/* never exits */
